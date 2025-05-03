cdbaaf2e731e046f4e1961b6981ee87bea320605
DanilSR <saidanilrai@gmail.com>
2025-05-03 15:53:49 +0300

    mp 9

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,6 +1,6 @@
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("привет, Git и GitHub!");
-        System.out.println("я начинаю изучение Java!!!!!!!!!!");
+        System.out.println("привет, Git и GitHub!!!");
+        System.out.println("Я начинаю изучение Java!!!!");
     }
 }
